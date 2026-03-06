96e928ee9136f90bd2bf0e73ec52e242f590c0ea
Manvir Sidhu <Pokemonstar5001@gmail.com>
2026-03-06 06:06:59 -0500

    Attempt 1

---
+++ components/ToDo.js
@@ -0,0 +1,95 @@
+class Todo {
+  constructor(data, selector) {
+    this.data = data;
+    this.selector = selector;
+    this.element = null;
+    this.nameEl = null;
+    this.dateEl = null;
+    this.checkboxEl = null;
+    this.labelEl = null;
+    this.deleteBtn = null;
+  }
+
+  _setEventListeners() {
+    const deleteBtn = this.element.querySelector(".todo__delete-btn");
+    if (this.deleteBtn) {
+      this.deleteBtn.addEventListener("click", () => this._deleteTodo());
+    }
+
+    const checkbox = this.element.querySelector(".todo__completed");
+    if (this.checkboxEl) {
+      this.checkboxEl.addEventListener("change", (evt) =>
+        this._toggleComplete(evt)
+    );
+  }
+
+  _deleteTodo() ,
+    this.element.remove();
+  }
+
+  _toggleComplete(evt) {
+    if (evt.target.checked) {
+      this.element.classList.add("todo_completed");
+    } else {
+      this.element.classList.remove("todo_completed");
+    }
+  }
+
+  _formatDueDate() {
+    if (!this.data.date) return "";
+    if (this.data.date instanceof Date) {
+      return this.data.date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      });
+    }
+  }
+
+  getView() {
+    const template = document.querySelector(this.selector);
+    if (!template) {
+      throw new Error(`Template with selector "${this.selector}" not found`);
+    }
+
+    const clone = template.content.cloneNode(true);
+    const todoElement = clone.querySelector(".todo");
+    if (!todoElement) {
+      throw new Error(`No element with class "todo" in template`);
+    }
+
+    this.element = todoElement;
+
+    this.nameEl = this.element.querySelector(".todo__name");
+    this.dateEl = this.element.querySelector(".todo__date");
+    this.checkboxEl = this.element.querySelector(".todo__completed");
+    this.labelEl = this.element.querySelector(".todo__label");
+    this.deleteBtn = this.element.querySelector(".todo__delete-btn");
+
+    if (this.nameEl) {
+      this.nameEl.textContent = this.data.name ?? "";
+}
+
+    
+      if (this.dateEl) {
+      this.dateEl.textContent = this._formatDueDate();
+    }
+
+
+     if (this.checkboxEl) {
+      this.checkboxEl.checked = !!this.data.completed;
+      checkbox.checked = !!this.data.completed;
+      const uniqueId = `todo-${crypto.randomUUID()}`;
+      this.checkboxEl.id = uniqueId;
+
+      if (this.labelEl) {
+        this.labelEl.setAttribute("for", uniqueId);
+    }
+
+    this.element = todoElement;
+    this._setEventListeners();
+    return this.element;
+  }
+}
+
+export default Todo;
